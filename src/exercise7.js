28182d9e964389535eec28c7d2cc9a5964db4d25
ricardosikic <ricardo.sikic@gmail.com>
2019-07-13 22:40:26 -0400

    terminados los ejercicios

--- src/exercise7.js
+++ src/exercise7.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import SimpleStorage from 'react-simple-storage';
 
 export class Practice7 extends React.Component {
+    state = {
+        count: 0
+    }
+    
+    increase = (e) => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    
+    decrease = (e) => {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
+    reset = () => {
+        this.setState({
+            count: 0
+        })
+    }
+
     render() {
         return(
-            <h1>hi</h1>
+            <Fragment>
+                <h4>add, decrement, reset</h4>
+                <SimpleStorage parent={this} />
+                <Header count={this.state.count}/>
+                <Button click={e => this.increase(e)} text='+'/>
+                <Button click={e => this.decrease(e)} text='-'/>
+                <Button click={e => this.reset(e)} text='reset' />
+            </Fragment>
         );
     }
+}
+
+const Header = (props) => {
+    return(
+        <Fragment>
+            <h3>{props.count}</h3>
+        
+        </Fragment>
+    );
+}
+
+// componente boton que almacenara las acciones que pasaran como props y que actualizaran al padre.
+// les pase los props como funciones y textos
+const Button = (props) => {
+    return(
+        <Fragment>
+            <button onClick={props.click}>{props.text}</button>
+           
+        </Fragment>
+    );
 }
